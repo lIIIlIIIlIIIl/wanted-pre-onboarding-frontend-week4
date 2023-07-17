af4c6ff6293467fddfc8d743901d8e94c98e3b10
ShinDongJae <sdhsdj6450@gmail.com>
2023-07-17 16:08:10 +0900

    feat: API 호출별로 로컬 캐싱 구현

--- client/src/hooks/useSearch.tsx
+++ client/src/hooks/useSearch.tsx
@@ -3,15 +3,48 @@ import { useEffect, useState } from "react";
 
 import { SickName, useApi } from "../context/APIContext";
 
+type CacheData = {
+  [key: string]: {
+    data: string[];
+    expiration: number;
+  };
+};
+
+const EXPIRATION_TIME = 1000 * 60;
+
 const useSearch = (word: string) => {
   const [searchList, setSearchList] = useState<SickName>([]);
+  const [cacheData, setCacheData] = useState<CacheData>({});
 
   const API = useApi();
 
-  const getSearchData = async (word: string) => {
-    const response = await API?.getSearch(word);
-    if (response) setSearchList(response.sickName);
-    console.info("calling api");
+  const getSearchData = (word: string) => {
+    if (cacheData[word] && cacheData[word].expiration > Date.now()) {
+      handleSearchData(cacheData[word].data);
+    } else {
+      return API?.getSearch(word)
+        .then((data) => {
+          if (data && data.sickName.length > 0) {
+            const expiration = Date.now() + EXPIRATION_TIME;
+            setCacheData((prev) => ({
+              ...prev,
+              [word]: {
+                data: data.sickName,
+                expiration,
+              },
+            }));
+            console.info("calling api");
+            setSearchList(data.sickName);
+          }
+        })
+        .catch((error) => {
+          console.error("API 호출 에러", error);
+        });
+    }
+  };
+
+  const handleSearchData = (data: string[]) => {
+    setSearchList(data);
   };
 
   useEffect(() => {
